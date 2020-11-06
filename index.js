571169fb72931cc2667da705dcd7e47fc4d6253f
Richard Meitern <richard.meitern@gmail.com>
2020-11-07 00:32:24 +0200

    refractored vtt definition

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function goToChapter(btnID){
-    var nextTimepoint = cue.data.nextChapter;
+function nextChapter(){
+    var nextChapter = cue.data.nextChapterID;
     cue.forcedExit = true;
+    var nextTimepoint = cues[nextChapter].startTime;
     video.currentTime = nextTimepoint;
+}
+
+function goToChapter(cueID){
+    var nextTimepoint = cues[cueID].startTime;
+    video.currentTime = nextTimepoint;
+}
+
+function parseRawCues(cues){
+    var cueArr = []; 
+    for (let index = 0; index < cues.length; index++) {
+        const cue = cues[index];
+        cueArr[cue.id] = cue;
+    }
+    
+    return(cueArr);
 }
 
 
@@ -11,20 +27,31 @@ if(Hls.isSupported()) {
     var activeOverlay;
     var activeOverlayClass;
     var clickedBtn;
+    var cuesLoaded = false;
+    var cues;
     var cue;
     var hls = new Hls({
         debug: false
     });
     hls.loadSource('media/est/master.m3u8');
     hls.attachMedia(video);
+
     hls.on(Hls.Events.MEDIA_ATTACHED, function() {
       video.muted = true;
-      video.currentTime = 7;
-      var metaTrack = video.textTracks[0];
-
+      video.currentTime = 0;
+      var metaTrack  = video.textTracks[0];
+      
+      
       
       metaTrack.mode = "hidden"; 
       metaTrack.oncuechange = function(e) {
+          if(!cuesLoaded){
+            cues = parseRawCues(this.cues);
+            console.log(cues);
+            cuesLoaded = true;
+          }
+          
+        
           cue = this.activeCues[0];
           if(cue){
               cue.data = JSON.parse(cue.text);
@@ -36,15 +63,23 @@ if(Hls.isSupported()) {
               }
               
               cue.onexit = function(e){
-                  if(this.data.endAction == "back2Start"){
+                  if(this.data.type == "loop"){
                     if(!this.forcedExit){
                         video.currentTime = this.startTime;
                     }
                     else{activeOverlay.className ="hidden";}
                     
                   }
-                  if(this.data.endAction == "startNext"){
-                    activeOverlay.className ="hidden";
+                  if(this.data.type == "intro"){
+                    if(activeOverlay){
+                        activeOverlay.className ="hidden";
+                    }
+                  }
+                  if(this.data.type == "question"){
+                    if(activeOverlay){
+                        activeOverlay.className ="hidden";
+                        goToChapter(this.parentCueID);
+                    }
                   }
                   
               }
